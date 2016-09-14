3b2f7d32253f7ef233ce4666ab0cde8d84fef393
ransomx <xransomx@gmail.com>
2016-09-14 18:52:59 +0200

    Some fix

--- MusicWeb/MusicWebDB.cs
+++ MusicWeb/MusicWebDB.cs
@@ -16,5 +16,6 @@ namespace MusicWeb
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Playlist> Playlists { get; set; }
         public DbSet<Song> Songs { get; set; }
+        public DbSet<Account> Users { get; set; }
     }
 }
---
+++ MusicWeb/Startup.cs
@@ -0,0 +1,17 @@
+﻿
+using Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.AspNet.Identity;
+using System.Configuration;
+
+namespace MusicWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
